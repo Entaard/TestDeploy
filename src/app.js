50b1d01cb09ec5fe59609c74120a2ecba7142add
Enta'ard <dratannta@gmail.com>
2017-02-04 16:33:03 +0700

    further attempt to fix deploy error (20)

--- src/app.js
+++ src/app.js
@@ -15,10 +15,10 @@ if (process.env.NODE_ENV !== 'production') {
 		noInfo: true,
 		publicPath: config.output.publicPath
 	}))
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, '..', 'index.html'))
+	})
 }
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '..', 'index.html'))
-})
 
 app.listen(port);
 
